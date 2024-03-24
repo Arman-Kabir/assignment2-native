e9e14c2122cbf5cf9ba9e28b43a4aea819363405
Arman-Kabir <armankabir8@gmail.com>
2024-03-24 17:16:03 +0600

    light font added and button half done

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
-import { useFonts, Inter_900Black, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+import { ActivityIndicator, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useFonts, Inter_900Black, Inter_400Regular, Inter_700Bold, Inter_300Light } from '@expo-google-fonts/inter';
 
 export default function App() {
 
   let [fontsLoaded] = useFonts({
     'Inter-Regular': Inter_400Regular,
     'Inter-Black': Inter_900Black,
-    'Inter-Bold': Inter_700Bold
+    'Inter-Bold': Inter_700Bold,
+    'Inter-Light': Inter_300Light
   });
 
   if (!fontsLoaded) {
@@ -45,18 +46,24 @@ export default function App() {
           </View>
 
           {/* Time */}
-          <View>
-
+          <View style={styles.time}>
+            <Text>
+              <Text style={styles.timeBig}>23:14</Text>
+              <Text style={styles.timeSmall}>BST</Text>
+            </Text>
           </View>
 
           {/* Location */}
           <View>
-
+            <Text style={styles.location}>In London, uk</Text>
           </View>
 
           {/* Button */}
           <View>
-
+            <TouchableOpacity onPress={{}} style={styles.button}>
+              <Text>More</Text>
+              <Image source={require('./assets/Group 3.png')}></Image>
+            </TouchableOpacity>
           </View>
 
         </View>
@@ -78,6 +85,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   parent: {
+    flex: 1,
     justifyContent: 'space-between',
     marginTop: 32,
     paddingHorizontal: 26
@@ -104,5 +112,40 @@ const styles = StyleSheet.create({
     fontFamily: 'Inter-Regular',
     letterSpacing: 3,
     lineHeight: 25
+  },
+  // Time
+  time: {
+    marginTop: 16
+  },
+  timeBig: {
+    fontFamily: 'Inter-Bold',
+    fontSize: 100,
+    letterSpacing: -2.5,
+    lineHeight: 100,
+    color: '#fff'
+  },
+  timeSmall: {
+    fontFamily: 'Inter-Light',
+    fontSize: 15,
+    color: '#fff'
+  },
+  // Location
+  location: {
+    textTransform: 'uppercase',
+    color: '#fff',
+    letterSpacing: 3,
+    fontSize: 15,
+    fontFamily: 'Inter-Bold'
+  },
+  // Button
+  button: {
+    flexDirection:'row',
+    gap:8,
+    alignItems:'center',
+    backgroundColor: '#fff',
+    color: '#000',
+    width: 115,
+    height: 39
   }
+
 });
