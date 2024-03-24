b64b2afaa581ff685f0b813ffa71262ce754b9fd
Arman-Kabir <armankabir8@gmail.com>
2024-03-24 14:51:54 +0600

    layout and upper View done

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_900Black, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    'Inter-Regular': Inter_400Regular,
+    'Inter-Black': Inter_900Black,
+    'Inter-Bold': Inter_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return (<ActivityIndicator />)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('./assets/bg1.png')} style={{ flex: 1 }}>
+      {/* Parent */}
+      <View style={styles.parent}>
+
+        {/* UpperView */}
+        <View style={styles.upperView}>
+          <View>
+            <Text style={{ fontFamily: 'Inter-Regular', color: '#fff', fontSize: 12, lineHeight: 22 }}>“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”</Text>
+            <Text style={{ fontFamily: 'Inter-Bold', color: '#fff', fontSize: 12, lineHeight: 22 }}>Ada Lovelace</Text>
+          </View>
+
+          <View>
+            <Image source={require('./assets/refresh.png')}></Image>
+          </View>
+        </View>
+
+        {/* LowerView */}
+        <View style={styles.LowerView}>
+
+          {/* greetings */}
+          <View>
+
+          </View>
+
+          {/* Time */}
+          <View>
+
+          </View>
+
+          {/* Location */}
+          <View>
+
+          </View>
+
+          {/* Button */}
+          <View>
+            
+          </View>
+
+        </View>
+
+        {/* Expanded part */}
+        <View>
+
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
 
@@ -17,4 +72,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  parent: {
+    justifyContent: 'space-between',
+    marginTop: 32,
+    paddingHorizontal: 26
+  },
+  upperView: {
+    flexDirection: 'row'
+  },
+  LowerView: {
+
+  }
 });
